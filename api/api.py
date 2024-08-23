8f1dc91442c0f67300ab91a5a36171c61ba56011
lham-ss <lham.staffsetter@gmail.com>
2024-08-23 10:22:29 -0600

    save

--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
 import time
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/api/v1/time')
 def get_current_time():
--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+
+    fetch('http://127.0.0.1:5000/api/v1/time').then(res => res.json()).then(data => {
+      setCurrentTime(data?.time);
+    });
+
+  } , []); // Empty array means that this effect will only run once (like componentDidMount in class components) 
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +29,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
